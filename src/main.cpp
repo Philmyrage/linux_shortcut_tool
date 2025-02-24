1be42c3c02d1064239af5d3f33121e478bc208cb
Phillip Wood <Phillipwood2023@outlook.com>
2025-02-23 21:51:44 -0500

    validating exe files

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,28 @@
 #include<fstream>
 #include<string>
 
+bool is_executable(const std::filesystem::path& path)
+{
+	if(std::filesystem::exists(path)){
+		std::cout << "exists" << std::endl;
+		const auto& permissions = std::filesystem::status(path).permissions();
+		return ((permissions & std::filesystem::perms::owner_exec) != std::filesystem::perms::none ||
+			(permissions & std::filesystem::perms::group_exec) != std::filesystem::perms::none ||
+			(permissions & std::filesystem::perms::others_exec) != std::filesystem::perms::none);
+	}
+	return false;
+}
+
 int main(int argc, char* argv[]){
-	std::cout << std::filesystem::current_path() << std::endl;
-	std::cout << "Hello World" << std::endl;
+
+	if(argc >= 2 && is_executable(argv[1])){
+		std::cout << "VALID EXECUTABLE" << std::endl;
+	}
+	else{
+		std::cout << argv[1] << std::endl;
+		std::cout << "INVALID EXECUTABLE" << std::endl;
+	}
+
+
 	return 0;
 }
