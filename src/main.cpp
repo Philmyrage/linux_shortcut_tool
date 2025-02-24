960a78ac68d06e7ecbc817770d5976ed2ce3beaa
Phillip Wood <Phillipwood2023@outlook.com>
2025-02-23 22:46:47 -0500

    Setup vscode, also fixed bug with validating executables.

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,33 @@
-#include<iostream>
-#include<filesystem>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <string>
 
-bool is_executable(const std::filesystem::path& path)
+const bool is_executable(const std::filesystem::path &path)
 {
-	if(std::filesystem::exists(path)){
+	if (std::filesystem::exists(path) && std::filesystem::is_regular_file(path))
+	{
 		std::cout << "exists" << std::endl;
-		const auto& permissions = std::filesystem::status(path).permissions();
-		return ((permissions & std::filesystem::perms::owner_exec) != std::filesystem::perms::none ||
-			(permissions & std::filesystem::perms::group_exec) != std::filesystem::perms::none ||
-			(permissions & std::filesystem::perms::others_exec) != std::filesystem::perms::none);
+		const auto &permissions = std::filesystem::status(path).permissions();
+		return (permissions & std::filesystem::perms::owner_exec) != std::filesystem::perms::none ||
+			   (permissions & std::filesystem::perms::group_exec) != std::filesystem::perms::none ||
+			   (permissions & std::filesystem::perms::others_exec) != std::filesystem::perms::none;
 	}
 	return false;
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
-	if(argc >= 2 && is_executable(argv[1])){
+	if (argc >= 2 && is_executable(argv[1]))
+	{
 		std::cout << "VALID EXECUTABLE" << std::endl;
 	}
-	else{
+	else
+	{
 		std::cout << argv[1] << std::endl;
 		std::cout << "INVALID EXECUTABLE" << std::endl;
 	}
-
 
 	return 0;
 }
