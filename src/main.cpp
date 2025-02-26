b15a45465bfd3f80d957aca4776ee84d9b1baf15
Phillip Wood <Phillipwood2023@outlook.com>
2025-02-26 17:27:38 -0500

    fixed build issue

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
 #include "CommandParser.hpp"
+#include "Shortcut.hpp"
 
 int main(int argc, char *argv[])
 {
 	// if no arguments was provided
 	// prompt the user for the information one at a time to get the data for the file.
 	// or when add the gui launch the gui app.
-
+	if (argc < 2)
+	{
+		std::cout << "No arguments provided" << std::endl;
+	}
 	CommandParser parser = CommandParser(argc, argv);
 
 	const auto &options = parser.get_options();
